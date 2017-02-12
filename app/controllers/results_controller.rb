4f112203e5f05710cf459bfa5d0cfc1c0044a8fb
Danielle Mullane <danielle.mullane@acumendigital.com>
2017-02-11 22:07:43 -0700

    Store and pass search history as array in params

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,10 @@
 class ResultsController < ApplicationController
   def index
-    search_obj = Search.new('denver')
+    @location = params['location_input'].upcase
+
+    search_obj = Search.new(@location)
+
+    @searches = params['searches'] ? JSON.parse(params['searches']) << @location : [@location]
 
     @gmap_hash = {"lat" => search_obj.latitude, "lng" => search_obj.longitude}
   end
